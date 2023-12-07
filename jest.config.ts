9cffd78dbefc66c85548623cdfef337e1d5e1ba1
Mike Pih <mike@swell.is>
2023-12-07 10:05:26 -0800

    jest config typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   clearMocks: true,
   preset: 'ts-jest',
   restoreMocks: true,
   testEnvironment: 'node',
 };
+
+export default config;
